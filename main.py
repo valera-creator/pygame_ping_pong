fc709b8ccbd8d79861d8ad2a0baa8500022552c5
Valera <valerylarionov06@gmail.com>
2024-09-13 20:11:18 +0300

    add files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
