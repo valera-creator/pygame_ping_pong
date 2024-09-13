8d93ef13212a17f958f3217ff6f417c026c3a96c
Valera <valerylarionov06@gmail.com>
2024-09-13 20:34:46 +0300

    отслеживание нажатий на стрелочки

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import pygame
 
-if __name__ == '__main__':
+
+def check_keyboard(event):
+    if event.key == pygame.K_DOWN:
+        return 'низ'
+    if event.key == pygame.K_UP:
+        return 'вверх'
+    if event.key == pygame.K_LEFT:
+        return 'лево'
+    if event.key == pygame.K_RIGHT:
+        return 'право'
+
+
+def main():
+    running = True
+    WIDTH, HEIGHT = 800, 600
+
     pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Максим Зайцев')
     pygame.display.flip()
 
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+                running = False
+            elif event.type == pygame.KEYUP:
+                key = check_keyboard(event)
+                print(key)
     pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
