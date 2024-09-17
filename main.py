30c4b93572dd139109ca54cbbb488c6b9b108fae
Valera <valerylarionov06@gmail.com>
2024-09-17 22:25:38 +0300

    игроки в виде квадрата

--- main.py
+++ main.py
@@ -12,15 +12,11 @@ def end_game():
     """функция для показа финального окна"""
 
 
-def check_keyboard(event):
-    if event.key == pygame.K_DOWN:
-        return 'вниз1'
-    if event.key == pygame.K_UP:
-        return 'вверх1'
-    if event.key == pygame.K_s:
-        return 'вниз1'
+def check_keyboard(event, player1, player2):
     if event.key == pygame.K_w:
-        return 'вверх2'
+        return 'управление 1 игрока'
+    if event.key == pygame.K_UP:
+        return 'управление 2 игрока'
 
 
 class Ball(pygame.sprite.Sprite):
@@ -32,6 +28,28 @@ class Ball(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = width // 2 - self.size // 2, height // 2 - self.size // 2
+
+    def update(self, x, y):
+        self.rect = self.rect.move(x, y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, color_image, width, height):
+        super().__init__(all_sprites, player_sprites)
+
+        self.color_image = color_image
+        self.size = 40
+        self.image = pygame.image.load(os.path.join('assets', 'images', f'{color_image}.png'))
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+
+        dict_coords = {
+            'violet': (width // 2 - width // 4 - 50, height // 2 - self.size // 2),
+            'yellow': (width // 2 + width // 4 + 50, height // 2 - self.size // 2)
+        }
+
+        self.rect.x, self.rect.y = dict_coords[color_image]
 
     def update(self, x, y):
         self.rect = self.rect.move(x, y)
@@ -51,7 +69,11 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     all_sprites = pygame.sprite.Group()
     ball_sprites = pygame.sprite.Group()
+    player_sprites = pygame.sprite.Group()
+
     Ball(width, height)
+    player1 = Player(color_image='violet', width=width, height=height)
+    player2 = Player(color_image='yellow', width=width, height=height)
 
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Максим Зайцев')
@@ -61,7 +83,7 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
                 running = False
             if event.type == pygame.KEYDOWN:
-                key = check_keyboard(event)
+                check_keyboard(event, player1, player2)
 
         screen.fill('black')
         all_sprites.draw(screen)
